a1f686ea87bd51b91bbed69ff0b94a8713ed3972
abhishek <abhisingh562002offc@gmail.com>
2023-08-06 13:17:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//i dont know why in Types t must be captial for working
+// this is use for making that auto writing intro !!
+var typed = new Typed(".text", {
+  strings: ["MERN Developer", "Cricketer", "Web Devloper"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDeplay: 1000,
+  loop: true,
+});
