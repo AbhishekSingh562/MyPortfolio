daf41a042e4916c65e8222d9b5d9e3f2b1950065
abhishek <abhisingh562002offc@gmail.com>
2023-08-14 23:01:01 +0530

    added some links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //i dont know why in Types t must be captial for working
 // this is use for making that auto writing intro !!
 var typed = new Typed(".text", {
-  strings: ["MERN STACK Devloper"],
+  strings: ["MERN STACK Developer", "Web Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDeplay: 1000,
